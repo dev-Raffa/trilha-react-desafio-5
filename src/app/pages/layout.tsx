50ea3a918228edb3e11bf11e1ce5d022e89d6b5c
Rafael da Conceição <mnraffaell@gmail.com>
2023-09-06 17:27:39 -0300

    initial commit

---
+++ src/app/pages/layout.tsx
@@ -0,0 +1,15 @@
+import StyledComponentsRegistry from "../utils/styled-components/registrity";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html>
+      <body>
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+      </body>
+    </html>
+  );
+}
