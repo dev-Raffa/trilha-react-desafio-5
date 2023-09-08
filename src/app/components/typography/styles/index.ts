4992f2a101f4fdb0d99a3114005bd2aac86e2407
Rafael da Conceição <113853780+dev-Raffa@users.noreply.github.com>
2023-09-08 06:47:25 +0000

    fix: typography components

--- src/app/components/typography/styles/index.ts
+++ src/app/components/typography/styles/index.ts
@@ -1,17 +1,41 @@
-import { styled } from "styled-components";
-import { textProps, titleProps } from "../types";
+"use client";
 
-export const StyledTitle = styled("h1")<titleProps>`
+import Link from "next/link";
+import { styled } from "styled-components";
+import { textLinkProps, textProps, titleProps } from "../types";
+
+export const StyledTitle = styled("h1").attrs<titleProps>(({ level }) => ({
+  as: `h${level}`,
+}))<titleProps>`
   user-select: none;
-  ${({ weight, lineheight }) => ` font: ${weight} ${lineheight};`}
+  ${({ weight }) => weight && `font-weight: ${weight};`}
+  ${({ lineheight }) => lineheight && ` line-height:${lineheight};`}
+  ${({ margin }) => margin && `margin: ${margin};`};
 `;
 
 export const StyledText = styled.p<textProps>`
   user-select: none;
-  text-decoration: ${({ decoration }) => decoration};
-  text-align: ${({ align }) => align};
-  margin: ${({ margin }) => margin};
-  ${({ color }) => `color: ${color};`}
-  ${({ weight }) => `font-weight: ${weight};`}
-  ${({ size }) => `font-size: ${size};`}
+  ${({ align }) => align && `text-align: ${align};`};
+  ${({ margin }) => margin && `margin: ${margin};`};
+  ${({ color }) => color && `color: ${color};`}
+  ${({ weight }) => weight && `font-weight: ${weight};`}
+  ${({ size }) => size && `font-size: ${size};`}
+  ${({ decoration }) =>
+    decoration
+      ? `text-decoration: ${decoration};`
+      : "text-decoration: none;  "};
+`;
+
+export const StyledLink = styled(Link)<textLinkProps>`
+  cursor:;
+  display: inline-block;
+  ${({ align }) => align && `text-align: ${align};`};
+  ${({ margin }) => margin && `margin: ${margin};`};
+  ${({ color }) => color && `color: ${color};`}
+  ${({ weight }) => weight && `font-weight: ${weight};`}
+  ${({ size }) => size && `font-size: ${size};`}
+  ${({ decoration }) =>
+    decoration
+      ? `text-decoration: ${decoration};`
+      : "text-decoration: none;  "};
 `;
